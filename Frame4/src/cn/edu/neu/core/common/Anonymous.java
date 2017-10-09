8bcca5ea126e18c7b5dd4647592a8396b4463c14
tingxi <32624452+tingxi@users.noreply.github.com>
2017-10-09 09:41:43 +0800

    first

--- Frame4/src/cn/edu/neu/core/common/Anonymous.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.neu.core.common;
-
-/**
- * 匿名接口，用于跳过登录拦截器
- */
-public interface Anonymous {
-
-}
--- Frame4/src/cn/edu/neu/core/common/CommonAnonymousAction.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.neu.core.common;
-
-/**
- * 通用匿名Action，可跳过登录判断
- */
-public class CommonAnonymousAction extends CommonBaseAction implements
-		Anonymous {
-
-}
